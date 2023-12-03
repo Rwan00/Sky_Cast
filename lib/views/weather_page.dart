6ef73e3ab8078a97997310800a3571523633a5b3
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-12-03 19:43:33 +0200

    add search bar

--- lib/views/weather_page.dart
+++ lib/views/weather_page.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../widgets/search_field.dart';
+
 class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: ,
-      
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color.fromRGBO(46, 51, 90, 1), Color.fromRGBO(28, 27, 51, 1)], 
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
+        body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(46, 51, 90, 1),
+            Color.fromRGBO(28, 27, 51, 1)
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
       ),
+      child: Column(children: [
+        const SizedBox(
+          height: 38,
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(
+                Icons.arrow_back,
+                size: 24,
+                color: Color.fromRGBO(235, 235, 245, 0.6),
+              ),
+            ),
+            Text(
+              "Weather",
+              style: GoogleFonts.aBeeZee(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+        SearchField(
+          hint: ' Search for a city or airport',
+          validator: (String? k) {},
+          widget: const Icon(
+            Icons.search,
+            color: Color.fromRGBO(235, 235, 245, 0.6),
+          ),
+        )
+      ]),
     ));
   }
 }
---
+++ lib/widgets/search_field.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SearchField extends StatelessWidget {
+  final String hint;
+  final Function(String)? onChanged;
+  final TextInputType? inputType;
+  final TextEditingController? controller;
+  final String? Function(String?) validator;
+  final Widget? widget;
+
+  const SearchField(
+      {
+      required this.hint,
+      this.onChanged,
+      this.inputType,
+      this.controller,
+      required this.validator,
+      this.widget,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Container(
+      
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(28, 27, 51, 1),
+            Color.fromRGBO(46, 51, 90, 1),
+             Color.fromRGBO(28, 27, 51, 1)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(10))
+      ),
+        margin: const EdgeInsets.only(top: 8),
+        width: double.infinity,
+        height: height * 0.04,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: TextFormField(
+                  validator: validator,
+                  controller: controller,
+                  keyboardType: inputType,
+                  onChanged: onChanged,
+                  autofocus: false,
+                  style: const TextStyle(fontSize: 20, color: Colors.black),
+                  cursorColor: Colors.blueGrey,
+                  decoration: InputDecoration(
+                    prefixIcon: widget,
+                    hintText: hint,
+                    hintStyle:
+                        GoogleFonts.aBeeZee(color: const Color.fromRGBO(235, 235, 245, 0.6),fontSize: 17,fontWeight: FontWeight.w400),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
